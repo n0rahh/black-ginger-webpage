182255ba9ccca4281dd14cbfd106dbfc231c4e45
Vlad <vlad.herasymovychh@gmail.com>
2024-09-13 07:39:23 +0200

    build: project init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app theme="dark" class="app">
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss">
+@import '@/styles/global';
+
+.app {
+  background-color: $background-color !important;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,33 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+import '@mdi/font/css/materialdesignicons.css';
+
+import router from './router';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'dark',
+    themes: {
+      dark: {
+        colors: {
+          primary: '#1E1E2F',
+          secondary: '#282A36',
+          tertiary: '#383A59',
+        },
+      },
+    },
+  },
+});
+
+const app = createApp(App);
+app.use(vuetify);
+app.use(router);
+app.mount('#app');
