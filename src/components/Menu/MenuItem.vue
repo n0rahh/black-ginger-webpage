44afe6983fec66126f607af50c80773b6d2585e0
Vlad <vlad.herasymovychh@gmail.com>
2024-09-13 07:54:33 +0200

    feat: add basic menu

---
+++ src/components/Menu/MenuItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-expansion-panel rounded="lg">
+    <v-expansion-panel-title>
+      <template v-slot>
+        <v-row no-gutters>
+          <v-col class="d-flex justify-start" cols="6">
+            <span class="text-h5">{{ title }}</span>
+          </v-col>
+          <v-col class="d-flex justify-end" cols="5">
+            <v-fade-transition leave-absolute>
+              <span class="text-h6 font-weight-bold">{{ price }} $</span>
+            </v-fade-transition>
+          </v-col>
+        </v-row>
+      </template>
+    </v-expansion-panel-title>
+    <v-expansion-panel-text>
+      <v-row no-gutters="">
+        <v-col cols="6" class="d-flex align-center">
+          <p class="text-h6">
+            {{ description }}
+          </p>
+        </v-col>
+        <v-col offset="1" cols="5" class="d-flex justify-end">
+          <v-img :aspect-ratio="1" :src="image"></v-img>
+        </v-col>
+      </v-row>
+    </v-expansion-panel-text>
+  </v-expansion-panel>
+</template>
+
+<script>
+export default {
+  name: 'MenuItem',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" class="text-center">
+        <h1>Sushi menu</h1>
+      </v-col>
+    </v-row>
+    <v-container class="d-flex justify-center menu">
+      <v-row>
+        <v-col cols="12">
+          <v-expansion-panels>
+            <MenuItem
+              v-for="(item, index) in sushiItems"
+              :key="index"
+              :title="item.title"
+              :description="item.description"
+              :price="item.price"
+              :image="item.image"
+            />
+          </v-expansion-panels>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-container>
+</template>
+
+<script>
+import MenuItem from '@/components/Menu/MenuItem.vue';
+import imagePhila from '@/assets/img/phila.jpg';
+import imageCal from '@/assets/img/california.jpg';
+import imageTuna from '@/assets/img/tuna.jpg';
+
+export default {
+  name: 'Home',
+  components: {
+    MenuItem,
+  },
+  data() {
+    return {
+      imagePhila,
+      sushiItems: [
+        {
+          title: 'Philadelphia Roll',
+          description:
+            'Salmon, avocado, cucumber, cream cheese, tobiko, and spicy mayo.',
+          price: 12.99,
+          image: imagePhila,
+        },
+        {
+          title: 'California Roll',
+          description: 'Crab, avocado, cucumber, and tobiko.',
+          price: 10.99,
+          image: imageCal,
+        },
+        {
+          title: 'Spicy Tuna Roll',
+          description: 'Tuna, cucumber, and spicy mayo.',
+          price: 11.99,
+          image: imageTuna,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.menu {
+  max-width: 900px;
+}
+</style>
