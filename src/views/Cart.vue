ab5dc575a3687c35bf06a6237932657c5206bd3b
Vlad <vlad.herasymovychh@gmail.com>
2024-09-15 09:10:47 +0200

    feat: add rwd to the cart view

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -10,42 +10,44 @@
       <v-card-text>
         <v-row>
           <v-col cols="12">
-            <v-list>
-              <v-list-item
-                v-for="item in cartItems"
-                :key="item.id"
-                class="mb-6"
-              >
-                <v-list-item-title>
-                  {{ item.title }}
-                </v-list-item-title>
-                <v-list-item-subtitle> {{ item.price }}$ </v-list-item-subtitle>
-                <v-spacer></v-spacer>
-                <template v-slot:append>
-                  <v-list-item-action>
-                    <v-btn
-                      color="tertiary"
-                      size="x-small"
-                      icon
-                      @click="decrementQuantity(item)"
-                    >
-                      <v-icon size="small">mdi-minus</v-icon>
-                    </v-btn>
-                    <div class="square">
-                      <span class="mx-4">{{ item.quantity }}</span>
-                    </div>
-                    <v-btn
-                      color="tertiary"
-                      size="x-small"
-                      icon
-                      @click="incrementQuantity(item)"
-                    >
-                      <v-icon size="small">mdi-plus</v-icon>
-                    </v-btn>
-                  </v-list-item-action>
-                </template>
-              </v-list-item>
-            </v-list>
+            <div
+              v-for="item in cartItems"
+              :key="item.id"
+              class="mb-6 list-item"
+            >
+              <div class="content">
+                <span class="text-h6">
+                  <span>{{ item.title }}</span>
+                  <span class="ml-4">{{ item.price }}$</span>
+                </span>
+              </div>
+              <div class="actions d-flex align-center">
+                <v-btn
+                  color="tertiary"
+                  size="x-small"
+                  icon
+                  @click="decrementQuantity(item)"
+                >
+                  <v-icon size="small">mdi-minus</v-icon>
+                </v-btn>
+                <div class="square">
+                  <span class="mx-4">{{ item.quantity }}</span>
+                </div>
+                <v-btn
+                  color="tertiary"
+                  size="x-small"
+                  icon
+                  @click="incrementQuantity(item)"
+                >
+                  <v-icon size="small">mdi-plus</v-icon>
+                </v-btn>
+              </div>
+            </div>
+          </v-col>
+        </v-row>
+        <v-row v-if="cartItems.length">
+          <v-col cols="12" class="py-0">
+            <v-divider opacity="1" />
           </v-col>
         </v-row>
         <v-row v-if="cartItems.length">
@@ -97,5 +99,33 @@ export default {
   border: 1px solid #ccc;
   border-radius: 4px;
   margin: 0 8px;
+}
+
+.list-item {
+  padding-bottom: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid #cccccc52;
+
+  &:last-child {
+    border-bottom: none;
+  }
+
+  @media (max-width: 480px) {
+    flex-direction: column;
+
+    .content {
+      display: flex;
+      align-items: center;
+      margin-bottom: 16px;
+      text-align: center;
+    }
+
+    .actions {
+      width: 100%;
+      justify-content: space-around;
+    }
+  }
 }
 </style>
