7f659dc0d148d966741f8a2e7b8f94f1876992c8
Vlad <vlad.herasymovychh@gmail.com>
2024-09-18 23:10:20 +0200

    feat: add cors handler

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,33 @@
 import { https } from 'firebase-functions';
 import axios from 'axios';
 import functions from 'firebase-functions';
+import cors from 'cors';
+
+const allowedOrigins = [
+  'https://black-ginger.web.app',
+  'http://localhost:5173',
+];
+
+const handleCors = cors({
+  origin: (origin, callback) => {
+    const isAllowed = allowedOrigins.includes(origin);
+    callback(null, isAllowed);
+  },
+});
 
 export const calculateDistance = https.onRequest(async (req, res) => {
-  const { origin, destination } = req.query;
+  handleCors(req, res, async () => {
+    const { origin, destination } = req.query;
 
-  const apiKey = functions.config().googlemaps.key;
+    const apiKey = functions.config().googlemaps.key;
 
-  try {
-    const response = await axios.get(
-      `https://maps.googleapis.com/maps/api/distancematrix/json?destinations=${encodeURIComponent(destination)}&origins=${encodeURIComponent(origin)}&units=metric&key=${apiKey}`,
-    );
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).send('Error calculating distance');
-  }
+    try {
+      const response = await axios.get(
+        `https://maps.googleapis.com/maps/api/distancematrix/json?destinations=${encodeURIComponent(destination)}&origins=${encodeURIComponent(origin)}&units=metric&key=${apiKey}`,
+      );
+      res.json(response.data);
+    } catch (error) {
+      res.status(500).send('Error calculating distance');
+    }
+  });
 });
